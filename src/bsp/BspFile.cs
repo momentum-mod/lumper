8cb01f77d786fbc70850938821a97a10489a9323
Tom Ashford <35813309+tsa96@users.noreply.github.com>
2021-01-12 06:51:19 +0000

    Initial commit
    BSP header parsing

---
+++ src/bsp/BspFile.cs
@@ -0,0 +1,74 @@
+﻿using MomBspTools.util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MomBspTools
+{
+    public class BspFile
+    {
+        private const int HEADER_LUMPS = 64;
+        private const int HEADER_SIZE = 1036;
+        private const int MAX_LUMPS = 128;
+
+        private string _file;
+        private string _ident;
+        private string _name;
+        private int _revision;
+        private int _version;
+
+        List<Lump> lumps = new List<Lump>(HEADER_LUMPS);
+
+        public BspFile()
+        {
+            
+        }
+
+        public BspFile(string path)
+        {
+            Load(path);
+        }
+
+        public void Load(string path)
+        {
+            _file = Path.GetFullPath(path);
+            _name = Path.GetFileNameWithoutExtension(path);
+
+            BinaryReader reader = new BinaryReader(File.OpenRead(_file));
+
+            _ident = StringUtils.UnmakeByteId(reader.ReadInt32());
+            _version = reader.ReadInt32();
+
+            Console.WriteLine("Reading BSP {0} with header ident {1} and version {2}", _file, _ident, _version);
+
+            for (int i = 0; i < HEADER_LUMPS; i++)
+            {
+                int ofs = reader.ReadInt32();
+                int len = reader.ReadInt32();
+                int vers = reader.ReadInt32();
+                int fourcc = reader.ReadInt32();
+                LumpType type = (LumpType) i;
+
+                Lump l = new Lump
+                {
+                    Index = i,
+                    Type = type,
+                    Offset = ofs,
+                    Length = len,
+                    Version = vers,
+                    FourCC = fourcc
+                };
+                lumps.Add(l);
+                Console.WriteLine("Reading {0}, offset {1}, length {2}, version {3}, fourcc {4}", Enum.GetName(type), ofs, len, vers, fourcc);
+            }
+        }
+
+        public List<Lump> GetLumps() { return lumps; }
+        
+        public Lump GetLump(LumpType lumpType)
+        {
+            return lumps.FirstOrDefault(x => x.Type == lumpType);
+        }
+    }
+}
---
+++ src/bsp/Lump.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomBspTools
+{
+    public class Lump
+    {
+        private int _index;
+        
+        public Lump()
+        {
+
+        }
+
+        public int Index { get; set; }
+        public LumpType Type { get; set; }
+        public int Offset { get; set; }
+        public int Length { get; set; }
+        public int Version { get; set; }
+        public int FourCC {  get; set; }
+    }
+}
