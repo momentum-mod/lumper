81ffb34092492edcddf17efcefd58a94568d3cf2
BIRD <xbird@t-online.de>
2023-05-26 15:45:05 +0200

    feat: change tab behavior
    Only open tabs on double click or enter/return key pressed.
    Also added a Hotkey to close a tab.

--- src/Lumper.UI/Views/Bsp/BspTreeView.axaml.cs
+++ src/Lumper.UI/Views/Bsp/BspTreeView.axaml.cs
@@ -1,5 +1,9 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Lumper.UI.ViewModels.Bsp;
 
 namespace Lumper.UI.Views.Bsp;
 
@@ -13,5 +17,33 @@ public partial class BspTreeView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    public async void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Enter:
+                OpenSelectedNode();
+                break;
+            case Key.Delete:
+                //todo
+                break;
+
+        }
+    }
+
+    public async void OnDoubleClick(object sender, TappedEventArgs e)
+    {
+        OpenSelectedNode();
+    }
+
+    private void OpenSelectedNode()
+    {
+        if (DataContext is not BspViewModel model)
+        {
+            throw new NotSupportedException();
+        }
+        model.Open(model.SelectedNode);
     }
 }
