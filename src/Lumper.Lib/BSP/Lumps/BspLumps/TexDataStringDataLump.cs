aa0f21d711d2175f5a8f50a81a3332ae9e84cd08
tsa96 <tom@trumpetdust.org>
2024-06-24 04:51:48 +0100

    refactor(lib): cleanup TexDataStringDataLump
    Can't take a ref to a property (cursed), so make the class implement a resize method

--- src/Lumper.Lib/BSP/Lumps/BspLumps/TexDataStringDataLump.cs
+++ src/Lumper.Lib/BSP/Lumps/BspLumps/TexDataStringDataLump.cs
@@ -10,26 +10,33 @@ using Newtonsoft.Json.Linq;
 
 public class TexDataStringDataLump(BspFile parent) : ManagedLump<BspLumpType>(parent)
 {
-    public static readonly Encoding TextureNameEncoding = Encoding.UTF8;
     [JsonConverter(typeof(ByteArrayJsonConverter))]
-    public byte[] Data;
+    private byte[] _data = null!;
+
+    public byte[] Data { get => _data; private set => _data = value; }
 
     public override void Read(BinaryReader reader, long length) => Data = reader.ReadBytes((int)length);
 
     public override void Write(Stream stream) => stream.Write(Data, 0, Data.Length);
 
-    public override bool Empty() => Data.Length <= 0;
+    public override bool Empty => Data.Length <= 0;
+
+    public void Resize(int newSize) => Array.Resize(ref _data, newSize);
 }
 
 public class ByteArrayJsonConverter : JsonConverter<byte[]>
 {
     public override void WriteJson(JsonWriter writer, byte[]? value, JsonSerializer serializer)
     {
-        var o = JArray.FromObject(value.Select(x => (short)x));
-        o.WriteTo(writer);
+        if (value is null)
+            return;
+
+        JArray.FromObject(value.Select(x => (short)x)).WriteTo(writer);
     }
 
-    public override byte[]? ReadJson(JsonReader reader, Type objectType, byte[]? existingValue, bool hasExistingValue, JsonSerializer serializer) => throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+    public override byte[] ReadJson(JsonReader reader, Type objectType, byte[]? existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+        => throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
 
     public override bool CanRead => false;
 }
