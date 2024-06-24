8e8c3380e2fa22f8c8b826adb26a7826c2fe44c4
tsa96 <tom@trumpetdust.org>
2024-06-24 06:44:51 +0100

    feat(ui): matchers
    Honestly not crazy about these but we'll probably use them more when we
    do search stuff for the pakfile explorer or improve the entity editor
    search.

---
+++ src/Lumper.UI/ViewModels/Matchers/GlobMatcherViewModel.cs
@@ -0,0 +1,11 @@
+namespace Lumper.UI.ViewModels.Matchers;
+
+using Lumper.UI.Models.Matchers;
+
+public class GlobMatcherViewModel : MatcherViewModel
+{
+    public override string Name => "Glob";
+
+    public override Matcher ConstructMatcher(string pattern)
+        => new GlobMatcher($"*{pattern}*", string.IsNullOrEmpty(pattern), true);
+}
