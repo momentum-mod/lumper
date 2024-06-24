0c47053dd0d8a35ebdfd63cdbe3190c41fa62477
tsa96 <tom@trumpetdust.org>
2024-06-24 06:21:52 +0100

    feat(ui): integrate NLog with Avalonia

--- src/Lumper.UI/Program.cs
+++ src/Lumper.UI/Program.cs
@@ -1,7 +1,10 @@
 namespace Lumper.UI;
+
 using System;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using NLog;
 
 internal sealed class Program
 {
@@ -9,12 +12,30 @@ internal sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // NLog isn't loading nlog.config unless I have this here. No idea why!
+        Logger.Info("Program started");
 
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+
+            Desktop.ShutdownRequested += (_, _) => LogManager.Shutdown();
+        }
+        catch (Exception e)
+        {
+            if (Debugger.IsAttached)
+                Debugger.Break();
+
+            LogManager.GetCurrentClassLogger().Fatal(e);
+        }
+    }
     // Avalonia configuration, don't remove; also used by visual designer.
     private static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
         .UsePlatformDetect()
         .LogToTrace()
         .UseReactiveUI();
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 }
