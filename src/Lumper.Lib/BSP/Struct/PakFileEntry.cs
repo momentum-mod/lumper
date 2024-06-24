637363f9d8ef6ad7beb2a574b291b83f168176f5
tsa96 <tom@trumpetdust.org>
2024-06-24 05:48:31 +0100

    fix(lib): thread-safe PakfileEntry streams
    UI can throw pretty easily if two threads try to access the underlying zip at the same time

--- src/Lumper.Lib/BSP/Struct/PakFileEntry.cs
+++ src/Lumper.Lib/BSP/Struct/PakFileEntry.cs
@@ -7,50 +7,98 @@ using System.Security.Cryptography;
 using Newtonsoft.Json;
 using SharpCompress.Archives.Zip;
 
-public class PakFileEntry
+/// <summary>
+/// A pakfile entry derived from either a ZipArchiveEntry or just a stream.
+/// We assume one is non-null throughout.
+/// </summary>
+public sealed class PakfileEntry : IDisposable
 {
-    public PakFileEntry(ZipArchiveEntry entry)
+    public PakfileEntry(ZipArchiveEntry zipEntry)
     {
-        _entry = entry;
-        Key = entry.Key;
+        ZipEntry = zipEntry;
+        Key = zipEntry.Key ?? throw new InvalidDataException("Pakfile contains an item without a key");
     }
 
-    public PakFileEntry(string key, Stream stream)
+    public PakfileEntry(string key, Stream stream)
     {
         Key = key;
-        _dataStream = stream;
+
+        stream.Seek(0, SeekOrigin.Begin);
+        var mem = new MemoryStream();
+        stream.CopyTo(mem);
+        _buffer = mem.ToArray();
     }
 
+    // Probably shouldn't be public but whatever. be careful!
     [JsonIgnore]
-    private readonly ZipArchiveEntry? _entry;
+    public ZipArchiveEntry? ZipEntry { get; }
 
     public string Key { get; set; }
 
-    [JsonIgnore]
-    private Stream? _dataStream;
+    [JsonProperty]
+    public long? CompressedSize => ZipEntry?.CompressedSize ?? null;
 
     [JsonIgnore]
-    public Stream DataStream
+    public bool IsModified { get; set; }
+
+    [JsonIgnore]
+    private byte[]? _buffer;
+
+    private readonly List<MemoryStream> _issuedStreams = [];
+
+    [JsonIgnore]
+    private readonly object _lock = new();
+
+    /// <summary>
+    /// Get an stream to the uncompressed pakfile entry.
+    ///
+    /// This stream can be invalidated if the entry is updated by the user.
+    /// </summary>
+    public MemoryStream GetReadOnlyStream()
     {
-        get => _dataStream ?? _entry!.OpenEntryStream(); // Ctors take either entry or stream, one is non-null
-        set
+        lock (_lock)
         {
-            if (_dataStream is not null)
-            {
-                _dataStream.Close();
-                _dataStream.Dispose();
-            }
-            _dataStream = value;
+            if (_buffer is not null)
+                return IssueStream();
+
+            using Stream zipStream = ZipEntry!.OpenEntryStream();
+            using var outStream = new MemoryStream();
+            zipStream.CopyTo(outStream);
+            _buffer = outStream.ToArray();
+            return IssueStream();
         }
     }
 
-    public bool IsModified => _dataStream != null;
+    private MemoryStream IssueStream()
+    {
+        var stream = new MemoryStream(_buffer!, writable: false);
+        _issuedStreams.Add(stream);
+        return stream;
+    }
 
+    public void UpdateData(byte[] data)
+    {
+        lock (_lock)
+        {
+            DisposeIssuedStreams();
+            _buffer = data;
+        }
+    }
+
+    private void DisposeIssuedStreams()
+    {
+        foreach (MemoryStream oldStream in _issuedStreams)
+            oldStream.Close();
+    }
+
+    public void Dispose() => DisposeIssuedStreams();
+
+    // Doesn't matter that this is unused, is used for JSON comparisons.
     public byte[] HashMd5
     {
         get
         {
-            Stream stream = DataStream;
+            Stream stream = GetReadOnlyStream();
             if (stream.CanSeek)
                 stream.Seek(0, SeekOrigin.Begin);
             return MD5.Create().ComputeHash(stream);
