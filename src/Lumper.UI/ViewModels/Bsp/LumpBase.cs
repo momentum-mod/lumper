f809b86268ffba6d948aceeef17eb49eef0f1a89
tsa96 <tom@trumpetdust.org>
2024-06-24 06:11:38 +0100

    chore(ui): delete old UI files
    As discussed, seems like it'll be easier to review if the stuff that's 80% rewritten
    if it's committed from scratch.

--- src/Lumper.UI/ViewModels/Bsp/LumpBase.cs
+++
@@ -1,6 +0,0 @@
-namespace Lumper.UI.ViewModels.Bsp;
-/// <summary>
-///     ViewModel base for all <see cref="Lib.BSP.Lumps.Lump" /> instances.
-/// </summary>
-public abstract class LumpBase(BspViewModel parent) : BspNodeBase(parent)
-{ }
