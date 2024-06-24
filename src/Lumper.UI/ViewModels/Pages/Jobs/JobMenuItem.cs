8b388e7b8f29ee51524018d8729974c613c9e766
tsa96 <tom@trumpetdust.org>
2024-06-24 06:48:55 +0100

    feat(ui): jobs page

---
+++ src/Lumper.UI/ViewModels/Pages/Jobs/JobMenuItem.cs
@@ -0,0 +1,18 @@
+namespace Lumper.UI.ViewModels.Pages.Jobs;
+
+using System;
+using Lib.Jobs;
+
+public interface IJobMenuItem<out T>; // Interface for covariance
+
+public class JobMenuItem<T>(JobsViewModel jobsVm, Func<T> factory) : IJobMenuItem<T> where T : IJob
+{
+    public string Name => T.JobName;
+
+    public void Create()
+    {
+        T job = factory();
+        JobViewModel jobVm = JobsViewModel.CreateJobViewModel(job);
+        jobsVm.Jobs.Add(jobVm);
+    }
+}
