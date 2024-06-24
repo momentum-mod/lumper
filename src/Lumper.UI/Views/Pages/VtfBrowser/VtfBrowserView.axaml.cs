d6a14362fa205b03b01baed5286fef8be67997c6
tsa96 <tom@trumpetdust.org>
2024-06-24 06:49:10 +0100

    feat(ui): vtf browser page

---
+++ src/Lumper.UI/Views/Pages/VtfBrowser/VtfBrowserView.axaml.cs
@@ -0,0 +1,9 @@
+namespace Lumper.UI.Views.Pages.VtfBrowser;
+
+using Avalonia.ReactiveUI;
+using ViewModels.Pages.VtfBrowser;
+
+public partial class VtfBrowserView : ReactiveUserControl<VtfBrowserViewModel>
+{
+    public VtfBrowserView() => InitializeComponent();
+}
