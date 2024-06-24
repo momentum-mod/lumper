66f0c3c9abd35ca6c147e857a287be1de6e28e95
tsa96 <tom@trumpetdust.org>
2024-06-24 06:11:38 +0100

    chore(ui): delete old UI files
    As discussed, seems like it'll be easier to review if the stuff that's 80% rewritten
    if it's committed from scratch.

--- src/Lumper.UI/ViewModels/Tasks/RunExternalToolTaskViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Lumper.UI.ViewModels.Tasks;
-using Lumper.Lib.Tasks;
-
-/// <summary>
-///     ViewModel for RunExternalTool Task
-/// </summary>
-public class RunExternalToolTaskViewModel(RunExternalToolTask task) : TaskViewModel(task)
-{ }
--- src/Lumper.UI/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-namespace Lumper.UI.ViewModels;
-using ReactiveUI;
-
-/// <summary>
-///     Base for all ViewModels
-/// </summary>
-public class ViewModelBase : ReactiveObject
-{ }
