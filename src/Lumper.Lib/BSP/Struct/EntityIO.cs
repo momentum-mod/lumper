28a2fff83579beca9dce2389ab386716ebfcf8a1
tsa96 <tom@trumpetdust.org>
2024-06-24 05:36:04 +0100

    refactor(lib): rework entities
    - EntityIO now detected consistently
    - Makes entities clonable (needed by raw entity view UI)
    - Stored in HashSet in EntityLump for UI performance
    - Can parse in a "strict" mode that'll throw if something is invalid

--- src/Lumper.Lib/BSP/Struct/EntityIO.cs
+++ src/Lumper.Lib/BSP/Struct/EntityIO.cs
@@ -4,14 +4,92 @@ using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public class EntityIO
+public partial class EntityIo : ICloneable
 {
     public string? TargetEntityName { get; set; }
     public string? Input { get; set; }
     public string? Parameter { get; set; }
-    public float Delay { get; set; }
-    public int TimesToFire { get; set; }
+    public float? Delay { get; set; }
+    public int? TimesToFire { get; set; }
 
-    public static bool IsIO(string value) => value.Count(s => s == ',') == 4;
-    public override string ToString() => $"{TargetEntityName},{Input},{Parameter},{Delay},{TimesToFire}";
+    private readonly char _separator;
+
+    public EntityIo() { }
+
+    public EntityIo(string value, char separator)
+    {
+        _separator = separator;
+        var props = value.Split(separator);
+
+        TargetEntityName = props[0];
+        Input = props[1];
+        Parameter = props[2];
+        Delay = float.Parse(props[3]);
+        TimesToFire = int.Parse(props[4]);
+    }
+
+    // Since VScript, \u001b (ESC) has been used as a separator.
+    // It'd be faster to determine this once and pass that information around,
+    // but would have to pass EntityLump into here or some other kind of spaghetti.
+    // Note that on Strata we apparently set the entity lump version to 1, and always
+    // use ESC, but again, don't want to pass more stuff around.
+    // Discussed in #strata-tools on 11 June 2024
+    public static bool TryParse(string value, out EntityIo? parsed, int elVersion = 0)
+    {
+        // ESC chars are deliberately hard to type so if we see 4, we're confident it's an ent IO, and it's def
+        // not an ESC char-based separator.
+        if (value.Count(s => s == '\u001b') == 4)
+        {
+            parsed = new EntityIo(value, '\u001b');
+            return true;
+        }
+
+        // If a new strata BSP the entity lump version is 1 and separator char is *always* an ESC
+        if (elVersion == 1)
+        {
+            parsed = null;
+            return false;
+        }
+
+        // Fast comparison, since most stuff isn't entio
+        if (value.Count(s => s == ',') != 4)
+        {
+            parsed = null;
+            return false;
+        }
+
+        // Finally do slow check. This is annoying to do but otherwise strings like
+        // a,b,c,d, can pass - bhop_flything had this issue.
+        if (EntityIoRegex().IsMatch(value))
+        {
+            parsed = new EntityIo(value, ',');
+            return true;
+        }
+        else
+        {
+            parsed = null;
+            return false;
+        }
+    }
+
+    public override string ToString()
+        => TargetEntityName +
+           _separator +
+           Input +
+           _separator +
+           Parameter +
+           _separator +
+           Delay +
+           _separator +
+           TimesToFire;
+
+    public object Clone() => MemberwiseClone();
+
+    // Match string,string,string,number,number where numbers can be decimals, negative, -.xxx
+    [GeneratedRegex(
+        @"^[^,]*," +
+        @"[^,]*," +
+        @"[^,]*" +
+        @"(,(-?\d*\.?\d+)?){2}$")]
+    private static partial Regex EntityIoRegex();
 }
