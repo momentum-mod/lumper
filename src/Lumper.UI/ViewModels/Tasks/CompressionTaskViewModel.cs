a0b89998f64aaef05ca6b7410e39ad9b6c3c406d
tsa96 <tom@trumpetdust.org>
2024-06-24 06:11:38 +0100

    chore(ui): delete old UI files
    As discussed, seems like it'll be easier to review if the stuff that's 80% rewritten
    if it's committed from scratch.

--- src/Lumper.UI/ViewModels/Tasks/CompressionTaskViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Lumper.UI.ViewModels.Tasks;
-using Lumper.Lib.Tasks;
-
-/// <summary>
-///     ViewModel for Compression Task
-/// </summary>
-public class CompressionTaskViewModel(CompressionTask task) : TaskViewModel(task)
-{ }
--- src/Lumper.UI/ViewModels/Tasks/StripperTaskViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Lumper.UI.ViewModels.Tasks;
-using Lumper.Lib.Tasks;
-
-/// <summary>
-///     ViewModel for Stripper Task
-/// </summary>
-public class StripperTaskViewModel(StripperTask task) : TaskViewModel(task)
-{ }
