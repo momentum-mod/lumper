f20a512765e57ee339bc38143b28fba95b6812cd
tsa96 <tom@trumpetdust.org>
2024-06-24 06:48:55 +0100

    feat(ui): jobs page

---
+++ src/Lumper.UI/Views/Pages/Jobs/JobsView.axaml.cs
@@ -0,0 +1,9 @@
+namespace Lumper.UI.Views.Pages.Jobs;
+
+using Avalonia.ReactiveUI;
+using ViewModels.Pages.Jobs;
+
+public partial class JobsView : ReactiveUserControl<JobsViewModel>
+{
+    public JobsView() => InitializeComponent();
+}
---
+++ src/Lumper.UI/Views/Pages/Jobs/RunExternalToolJobView.axaml.cs
@@ -0,0 +1,9 @@
+namespace Lumper.UI.Views.Pages.Jobs;
+
+using Avalonia.ReactiveUI;
+using ViewModels.Pages.Jobs;
+
+public partial class RunExternalToolJobView : ReactiveUserControl<RunExternalToolJobViewModel>
+{
+    public RunExternalToolJobView() => InitializeComponent();
+}
