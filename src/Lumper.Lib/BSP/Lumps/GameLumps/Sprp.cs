db0c7aabad74e69a74f2583a8fb7ef338a80e5f7
tsa96 <tom@trumpetdust.org>
2024-07-02 19:17:30 +0100

    fix(lib): failing to switch to staticprop V7s in rare cases
    Closes #46
    
    For some certain values the former code would think a static prop lump
    was a valid V10 when it's actually a V7S.
    
    Take a V7s with 19 entries: length = 72 * 19 = 1368
    But 1368 / 76 = 18, so remainingLength % structsize = 0.
    But this won't actually pass the below remainingLength == entries * structsize check.

--- src/Lumper.Lib/BSP/Lumps/GameLumps/Sprp.cs
+++ src/Lumper.Lib/BSP/Lumps/GameLumps/Sprp.cs
@@ -33,7 +33,7 @@ public class Sprp(BspFile parent) : ManagedLump<GameLumpType>(parent)
 
         if (StaticProps.ActualVersion is StaticPropVersion.V7 or StaticPropVersion.V10)
         {
-            if (remainingLength % StaticProps.StructureSize != 0)
+            if (remainingLength / StaticProps.StructureSize != entries)
             {
                 StaticProps.ActualVersion = StaticPropVersion.V7s;
                 Logger.Warn($"Remaining length of staticprop lumpdoesn't fit version {Version}, trying V7s");
