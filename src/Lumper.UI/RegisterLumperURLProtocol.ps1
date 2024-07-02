8ebdde861b9dd9590220f57d21dd21d4f1139ec5
tsa96 <tom@trumpetdust.org>
2024-07-02 01:15:06 +0100

    feat(ui): launch lumper via url protocol

---
+++ src/Lumper.UI/RegisterLumperURLProtocol.ps1
@@ -0,0 +1,20 @@
+﻿if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as an administrator."
+    exit
+}
+
+$protocolName = "lumper"
+$exePath = Join-Path $PWD "Lumper.UI.exe"
+$registryPath = "Registry::HKEY_CLASSES_ROOT\$protocolName"
+
+try {
+    New-Item -Path $registryPath -Force
+    Set-ItemProperty -Path $registryPath -Name "URL Protocol" -Value ""
+    New-Item -Path "$registryPath\shell\open\command" -Force
+    Set-ItemProperty -Path "$registryPath\shell\open\command" -Name "(Default)" -Value "`"$exePath`" `"%1`""
+
+    Write-Host "Custom URL protocol '$protocolName' registered successfully."
+} catch {
+    Write-Host "Error occurred: $_"
+}
+
