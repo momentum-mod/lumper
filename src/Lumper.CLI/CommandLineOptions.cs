29eb1ee00ad2493a399b987ec485dccf9683a8cf
tsa96 <tom@trumpetdust.org>
2024-07-23 15:11:31 +0100

    refactor(cli): rework existing CLI program
    - Separates out parser logic from actual BSP-related code
    - Uses NLog
    - Prints usage better
    - Catches and prints Lumper.Lib errors with exit code handling
    - Less arg checking, let CommandLine handle it

--- src/Lumper.CLI/CommandLineOptions.cs
+++ src/Lumper.CLI/CommandLineOptions.cs
@@ -2,40 +2,89 @@ namespace Lumper.CLI;
 
 using CommandLine;
 
+[Flags]
 public enum JsonOptions
 {
-    Default,
-    SortLumps,
-    SortProperties,
-    IgnoreOffset,
+    SortLumps = 0x1,
+    SortProperties = 0x2,
+    IgnoreOffset = 0x4,
 }
 
 public class CommandLineOptions
 {
-    [Value(index: 0, Required = false, HelpText = "BSP file path")]
-    public string? Path { get; set; }
-
-    private const string JsonOptionName = "json";
+    [Value(index: 0, Required = true, MetaName = "Input BSP", HelpText = "Path to input BSP file")]
+    public required string InputPath { get; set; }
 
     [Option(
-        JsonOptionName,
-        Separator = ',',
-        // Required because its the only thing the cli can do for now :)
-        Required = true,
-        HelpText = "Export to JSON (for comparison)"
+        'o',
+        "output",
+        Required = false,
+        HelpText = "Path to output BSP file. If not provided, and some action modifies the BSP, the input BSP will be overwritten."
     )]
-    public IEnumerable<JsonOptions>? Json { get; set; }
+    public string? OutputPath { get; set; }
 
-    public static void ErrorHandler(IEnumerable<Error> errors)
-    {
-        if (
-            errors.Any(x =>
-                x is BadFormatConversionError or MissingValueOptionError
-                && ((NamedError)x).NameInfo.LongName == JsonOptionName
-            )
-        )
-        {
-            Console.WriteLine("Available JSON options: " + string.Join(", ", Enum.GetNames(typeof(JsonOptions))));
-        }
-    }
+    [Option('c', "compress", SetName = "compress", Required = false, HelpText = "Save the output BSP file compressed.")]
+    public bool Compress { get; set; }
+
+    [Option(
+        'C',
+        "noCompress",
+        SetName = "compress",
+        Required = false,
+        HelpText = "Save the output BSP file uncompressed."
+    )]
+    public bool DontCompress { get; set; }
+
+    [Option(
+        'B',
+        "skipBackup",
+        Required = false,
+        Default = false,
+        HelpText = "Don't make a backup of the input BSP file before overwriting it."
+    )]
+    public bool SkipBackup { get; set; }
+
+    [Option(
+        'w',
+        "workflow",
+        Required = false,
+        HelpText = "Path to a JSON file containing a job workflow to apply to the BSP file."
+    )]
+    public string? JobWorkflow { get; set; }
+
+    [Option(
+        'j',
+        "json",
+        Default = false,
+        Required = false,
+        HelpText = "Output JSON summary of the BSP to <directory containing BSP file>/<bsp file name>.json"
+    )]
+    public bool JsonDump { get; set; }
+
+    [Option(
+        'J',
+        "jsonPath",
+        Required = false,
+        HelpText = "Output JSON summary of the BSP the given path, relative to current directory. If given, --json can be omitted."
+    )]
+    public string? JsonPath { get; set; }
+
+    [Option(
+        'k',
+        "jsonOpts",
+        Default = CLI.JsonOptions.SortLumps | CLI.JsonOptions.SortProperties | CLI.JsonOptions.IgnoreOffset,
+        Required = false,
+        HelpText = "Provide either flags (1 = Sort Lumps, 2 = Sort Properties, 4 = Ignore Offsets),"
+            + " or comma-separator list of names, e.g. sortproperties,ignoreoffsets."
+    )]
+    public JsonOptions JsonOptions { get; set; }
+
+    [Option(
+        'v',
+        "--verbose",
+        Default = false,
+        Required = false,
+        HelpText = "Enable verbose output (include debug messages)"
+    )]
+    public bool Verbose { get; set; }
 }
