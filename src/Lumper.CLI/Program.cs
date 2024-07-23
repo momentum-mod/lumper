41221c0cdab786af458c418ab0b3fa46eefdc652
tsa96 <tom@trumpetdust.org>
2024-07-23 15:11:31 +0100

    refactor(cli): rework existing CLI program
    - Separates out parser logic from actual BSP-related code
    - Uses NLog
    - Prints usage better
    - Catches and prints Lumper.Lib errors with exit code handling
    - Less arg checking, let CommandLine handle it

--- src/Lumper.CLI/Program.cs
+++ src/Lumper.CLI/Program.cs
@@ -1,32 +1,182 @@
 namespace Lumper.CLI;
 
+using System.Diagnostics;
 using CommandLine;
+using CommandLine.Text;
 using Lumper.Lib.Bsp;
+using Lumper.Lib.Bsp.Enum;
+using Lumper.Lib.Jobs;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 internal sealed class Program
 {
-    public static void Main(string[] args)
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    private static bool _shouldWriteOutput;
+
+    public static int Main(string[] args)
     {
-        var parser = new Parser(with =>
+        LoggingRule? consoleRule = LogManager.Configuration?.LoggingRules?.FirstOrDefault(rule =>
+            rule.Targets.FirstOrDefault() is ColoredConsoleTarget
+        );
+        if (LogManager.Configuration is null || consoleRule is null)
         {
-            with.HelpWriter = Console.Out;
-            with.CaseInsensitiveEnumValues = true;
-        });
-        ParserResult<CommandLineOptions> parserResult = parser.ParseArguments<CommandLineOptions>(args);
-
-        string? path = null;
-        parserResult.WithParsed(x => path = x.Path).WithNotParsed(CommandLineOptions.ErrorHandler);
-        if (path == null)
-            return;
-
-        if (parserResult.Value.Json != null && parserResult.Value.Json.Any())
-        {
-            var bspFile = BspFile.FromPath(path, null);
-
-            bool sortLumps = parserResult.Value.Json.Any(x => x == JsonOptions.SortLumps);
-            bool sortProperties = parserResult.Value.Json.Any(x => x == JsonOptions.SortProperties);
-            bool ignoreOffset = parserResult.Value.Json.Any(x => x == JsonOptions.IgnoreOffset);
-            bspFile?.JsonDump((string?)null, null, sortLumps, sortProperties, ignoreOffset);
+            Console.Error.WriteLine(
+                "Could not find logging configuration! You should have a NLog.config in the same directory as this executable."
+            );
+            return 1;
         }
+
+        // Remove date and other crap from layout. This is good for file logs but here
+        // we want to just print messages directly.
+        // https://github.com/NLog/NLog/wiki/Layouts
+        consoleRule.Targets.OfType<ColoredConsoleTarget>().First().Layout = "${message:withException=true}";
+
+        ParserResult<CommandLineOptions> parserResult = new Parser(with =>
+        {
+            with.CaseInsensitiveEnumValues = true;
+            with.HelpWriter = null;
+            with.AutoHelp = true;
+            with.AutoVersion = true;
+        }).ParseArguments<CommandLineOptions>(args);
+
+        if (parserResult.Errors.Any())
+            return ShowHelp(parserResult);
+
+        CommandLineOptions options = parserResult.Value;
+
+        consoleRule.SetLoggingLevels(options.Verbose ? LogLevel.Debug : LogLevel.Info, LogLevel.Fatal);
+        try
+        {
+            Run(options);
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+            return 1;
+        }
+    }
+
+    // The main application logic of the program, called once the command line options have been parsed.
+    //
+    // It can throw at any point, and the exception will be caught and program will exit with non-zero
+    // status code.
+    //
+    // If method completes successfully, the program will exit with status code 0.
+    private static void Run(CommandLineOptions options)
+    {
+        var bspFile = BspFile.FromPath(options.InputPath, null);
+
+        if (bspFile is null)
+            throw new InvalidDataException("Failed to load BSP file");
+
+        if (options.JobWorkflow is { } workflowPath)
+            RunWorkflow(bspFile, workflowPath);
+
+        if (_shouldWriteOutput || options.Compress || options.DontCompress)
+        {
+            DesiredCompression compression = DesiredCompression.Unchanged;
+            if (options.Compress)
+                compression = DesiredCompression.Compressed;
+            else if (options.DontCompress)
+                compression = DesiredCompression.Uncompressed;
+
+            bspFile.SaveToFile(options.OutputPath ?? null, compression, null, !options.SkipBackup);
+        }
+
+        if (options.JsonDump || options.JsonPath is not null)
+            JsonDump(bspFile, options);
+    }
+
+    private static void JsonDump(BspFile bspFile, CommandLineOptions options) =>
+        bspFile.JsonDump(
+            options.JsonPath ?? null,
+            null,
+            sortLumps: options.JsonOptions.HasFlag(JsonOptions.SortLumps),
+            sortProperties: options.JsonOptions.HasFlag(JsonOptions.SortProperties),
+            ignoreOffset: options.JsonOptions.HasFlag(JsonOptions.IgnoreOffset)
+        );
+
+    private static void RunWorkflow(BspFile bspFile, string workflowPath)
+    {
+        if (!File.Exists(workflowPath))
+        {
+            Logger.Warn("Could not find workflow file");
+            return;
+        }
+
+        using FileStream stream = File.OpenRead(workflowPath);
+        if (!Job.TryLoadWorkflow(stream, out List<Job>? workflow))
+            return; // TryLoadWorkflow logs any exceptions
+
+        Logger.Info("Loaded workflow file");
+        foreach (Job job in workflow)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Logger.Info($"Running job: {job.JobNameInternal}");
+
+            if (job.Run(bspFile))
+                _shouldWriteOutput = true;
+
+            stopwatch.Stop();
+            Logger.Info($"Job completed in {stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+
+    private static int ShowHelp(ParserResult<CommandLineOptions> parserResult)
+    {
+        // ReSharper disable PossibleMultipleEnumeration
+        IEnumerable<Error>? errors = parserResult.Errors;
+        string version = typeof(Program).Assembly.GetName().Version?.ToString() ?? "Unknown Version";
+        if (errors.IsVersion())
+        {
+            Logger.Info($"Lumper CLI v{version}");
+            return 0;
+        }
+
+        Logger.Info(
+            HelpText.AutoBuild(
+                parserResult,
+                h =>
+                {
+                    h.Heading = $"Lumper CLI v{version} [https://github.com/momentum-mod/lumper]";
+                    h.AddNewLineBetweenHelpSections = false;
+                    h.AdditionalNewLineAfterOption = false;
+                    h.Copyright = "";
+                    h.MaximumDisplayWidth = 100;
+                    // Custom comparator to put values above options - by default options are first
+                    // which I hate!!!
+                    h.OptionComparison = (a, b) =>
+                        a.IsOption switch
+                        {
+                            true when b.IsOption => a.Required switch
+                            {
+                                true when !b.Required => -1,
+                                false when b.Required => 1,
+                                _ => 0,
+                            },
+                            true when b.IsValue => 1,
+                            _ => -1,
+                        };
+                    h.AddPostOptionsLines(
+                        [
+                            "Without options, Lumper will simply read the BSP file and exit.",
+                            "If any options provided cause modifications, the BSP will be output to the same path.",
+                            "If you want to output to a different path, use the -o option.",
+                            "If -c or -C are given, an output file will always be produced, even if unmodified.",
+                            "If neither -c or -C are given and a BSP is output, BSP will be written in its current state.",
+                        ]
+                    );
+                    return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+                },
+                e => e
+            )
+        );
+
+        return errors.IsHelp() ? 0 : 1;
     }
 }
