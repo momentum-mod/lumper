eff458106b82b3954691e86d668095dcd0fec076
tsa96 <tom@trumpetdust.org>
2024-12-19 01:47:40 +0000

    feat(all): official asset removal job

---
+++ src/Lumper.Lib/Jobs/RemoveAssetJob.cs
@@ -0,0 +1,67 @@
+namespace Lumper.Lib.Jobs;
+
+using System.Collections.Generic;
+using System.Linq;
+using Lumper.Lib.Bsp;
+using Lumper.Lib.Bsp.Lumps.BspLumps;
+using Lumper.Lib.Bsp.Struct;
+using Lumper.Lib.Util;
+using NLog;
+
+public class RemoveAssetJob : Job, IJob
+{
+    public static string JobName => "Remove Game Assets";
+    public override string JobNameInternal => JobName;
+
+    /// <summary>
+    /// List of origins to remove matching assets from. If null, all matching assets are removed.
+    /// The typical use-case for this job is removing *all* assets; if we filled this list with
+    /// every current origin, the workflow would become incomplete as sloo
+    ///
+    /// </summary>
+    public List<string>? OriginFilter { get; set; } = [];
+
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public override bool Run(BspFile bsp)
+    {
+        if (OriginFilter?.Count == 0)
+        {
+            Logger.Warn("No games selected.");
+            return false;
+        }
+
+        PakfileLump pakfileLump = bsp.GetLump<PakfileLump>();
+
+        var numMatches = 0;
+
+        // Could probably speed this up a bit by parallelizing, but we can't read
+        // multiple zip entries at a time, and that's the most expensive operation here.
+        foreach (PakfileEntry entry in pakfileLump.Entries.ToList())
+        {
+            var hash = entry.HashSHA1;
+            if (!AssetManifest.Manifest.TryGetValue(hash, out List<AssetManifest.Asset>? assets))
+                continue;
+
+            if (OriginFilter is not null && !assets.Any(asset => OriginFilter.Contains(asset.Origin)))
+                continue;
+
+            pakfileLump.Entries.Remove(entry);
+            numMatches++;
+            var matches = string.Join(", ", assets.Select(asset => $"{asset.Origin} asset {asset.Path}"));
+            Logger.Info($"Removed {entry.Key} which matched {matches}");
+        }
+
+        if (numMatches > 0)
+        {
+            Logger.Info($"Removed {numMatches} game assets!");
+            pakfileLump.IsModified = true;
+            return true;
+        }
+        else
+        {
+            Logger.Info("Did not find any game assets to remove.");
+            return false;
+        }
+    }
+}
---
+++ src/Lumper.UI/ViewModels/Pages/Jobs/RemoveAssetJobViewModel.cs
@@ -0,0 +1,49 @@
+namespace Lumper.UI.ViewModels.Pages.Jobs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lumper.Lib.Jobs;
+using Lumper.Lib.Util;
+using Lumper.UI.Services;
+using Lumper.UI.ViewModels.Shared.Pakfile;
+using Lumper.UI.Views.Pages.Jobs;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+public sealed class RemoveAssetJobViewModel : JobViewModel
+{
+    public class GameSelection : ReactiveObject
+    {
+        public required string Origin { get; init; }
+
+        [Reactive]
+        public bool Selected { get; set; }
+    }
+
+    public List<GameSelection> Selection { get; } =
+        AssetManifest.Origins.Select(origin => new GameSelection { Origin = origin, Selected = true }).ToList();
+
+    public override RemoveAssetJob Job { get; }
+
+    public RemoveAssetJobViewModel(RemoveAssetJob job)
+        : base(job)
+    {
+        RegisterView<RemoveAssetJobViewModel, RemoveAssetJobView>();
+
+        Job = job;
+
+        foreach (GameSelection selection in Selection)
+            selection
+                .WhenAnyValue(x => x.Selected)
+                .Subscribe(_ =>
+                {
+                    var selected = Selection.Where(x => x.Selected).ToList();
+                    // If all are selected, set filter to null.
+                    Job.OriginFilter =
+                        selected.Count == AssetManifest.Origins.Count ? null : selected.Select(x => x.Origin).ToList();
+                });
+    }
+
+    protected override void OnSuccess() => BspService.Instance.ResetLumpViewModel(typeof(PakfileLumpViewModel));
+}
