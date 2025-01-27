ecb5c5497918eedf38cc92450f4630281ea1531a
tsa96 <tom@trumpetdust.org>
2025-01-27 12:05:02 +0000

    feat(ui): about dialog

---
+++ src/Lumper.UI/Views/AboutDialog.axaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Lumper.UI.Views;
+
+using System.Reflection;
+using Avalonia.ReactiveUI;
+using Services;
+using ViewModels;
+
+public partial class AboutWindow : ReactiveWindow<ViewModel>
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+
+        UpdaterService.SemVer version = UpdaterService.GetAssemblyVersion();
+
+        LumperVersion.Text = $"Lumper v{version}";
+        DotnetVersion.Text = $".NET {System.Environment.Version}";
+        AvaloniaVersion.Text =
+            $"Avalonia {Assembly.Load("Avalonia").GetName().Version?.ToString() ?? "Unknown Version"}";
+
+        CloseButton.Click += (_, _) => Close();
+    }
+}
