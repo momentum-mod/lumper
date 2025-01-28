1217fb8cf0e1b01342ed8907ae8ac7bd3bb745bd
tsa96 <tom@trumpetdust.org>
2025-01-28 07:10:10 +0000

    feat: StateService data persistence service

--- src/Lumper.UI/App.axaml.cs
+++ src/Lumper.UI/App.axaml.cs
@@ -1,10 +1,12 @@
 namespace Lumper.UI;
 
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using Lumper.UI.Services;
 using Lumper.UI.ViewModels;
 using Lumper.UI.Views;
+using ReactiveUI;
 
 public class App : Application
 {
@@ -12,9 +14,29 @@ public class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            desktop.MainWindow = new MainWindow { DataContext = new MainWindowViewModel() };
+        // Data persistence logic based on
+        // https://docs.avaloniaui.net/docs/concepts/reactiveui/data-persistence#creating-the-suspension-driver
+        // https://www.reactiveui.net/docs/handbook/data-persistence.html
+        //
+        // Avalonia page mentions viewmodel "tree", which would require hierarchical
+        // [DataContract]/[DataMember]s of viewmodels as properties of their parents,
+        // something like that. Seems very awkward to make work with lazy page loading
+        // stuff.
+        // We don't need to persist very much anyway, so just using a single "StateService"
+        // singleton that stores everything we want to persist.
 
+        // Initialize suspension helper using application lifetime
+        var suspension = new AutoSuspendHelper(Program.Desktop);
+
+        // Instantiate instance of StateService which will register itself as the static
+        // StateService.Instance property. `() => StateService.Instance()` doesn't work
+        // unfortunately.
+        RxApp.SuspensionHost.CreateNewAppState = () => new StateService();
+        RxApp.SuspensionHost.SetupDefaultSuspendResume(new JsonSuspensionDriver("appstate.json"));
+        suspension.OnFrameworkInitializationCompleted();
+
+        // Init main window
+        Program.Desktop.MainWindow = new MainWindow { DataContext = new MainWindowViewModel() };
         base.OnFrameworkInitializationCompleted();
     }
 }
---
+++ src/Lumper.UI/Services/StateService.cs
@@ -0,0 +1,20 @@
+﻿namespace Lumper.UI.Services;
+
+using System.Runtime.Serialization;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+/// <summary>
+/// Singleton for storing any state we want to persist between sessions.
+/// </summary>
+[DataContract]
+public class StateService : ReactiveObject
+{
+    // This is instantiated before the app window even opens, safe to `null!`
+    public static StateService Instance { get; private set; } = null!;
+
+    // Nasty public ctor needed for RxApp.SuspensionHost.CreateNewAppState
+    // `() => StateService.Instance` seems to result in multiple instances
+    // somehow??
+    public StateService() => Instance = this;
+}
