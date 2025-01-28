30577bd0c1bd4c0c5492ca8f97bf31a26a430031
tsa96 <tom@trumpetdust.org>
2025-01-28 07:11:26 +0000

    refactor(ui): store a bunch of checkbox states in StateService

--- src/Lumper.UI/Services/StateService.cs
+++ src/Lumper.UI/Services/StateService.cs
@@ -17,4 +17,19 @@ public class StateService : ReactiveObject
     // `() => StateService.Instance` seems to result in multiple instances
     // somehow??
     public StateService() => Instance = this;
+
+    [DataMember, Reactive]
+    public bool SaveCompressed { get; set; } = false;
+
+    [DataMember, Reactive]
+    public bool MakeBackup { get; set; } = true;
+
+    [DataMember, Reactive]
+    public bool LogShowDebug { get; set; } = false;
+
+    [DataMember, Reactive]
+    public bool LogAutoScroll { get; set; } = true;
+
+    [DataMember, Reactive]
+    public bool VtfBrowserShowCubemaps { get; set; } = false;
 }
