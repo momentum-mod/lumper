265fe1bc22b94dff6331fb0d317c8842ba532d87
tsa96 <tom@trumpetdust.org>
2025-01-30 19:47:47 +0000

    fix(lib): remove excess pakfileentry buffer padding
    Buffer is derived from a resizable MemoryStream buffer, so gets padded
    up. For text data, this results in a bunch of a NULs. Buffer is readonly
    unless replaced entirely, so we can just limit it to the size of the
    uncompressed zip data.

--- src/Lumper.Lib/BSP/Struct/PakFileEntry.cs
+++ src/Lumper.Lib/BSP/Struct/PakFileEntry.cs
@@ -74,7 +74,7 @@ public sealed class PakfileEntry
         {
             lock (ZipAccessLock)
             {
-                using var mem = new MemoryStream(); // Note MemoryStream disposal doesn't delete underlying buffer
+                using var mem = new MemoryStream((int)ZipEntry!.Size); // Note MemoryStream disposal doesn't delete underlying buffer
                 using Stream zipStream = ZipEntry!.OpenEntryStream();
                 zipStream.CopyTo(mem);
                 _buffer = mem.GetBuffer();
