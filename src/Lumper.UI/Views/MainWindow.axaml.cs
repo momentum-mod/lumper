9edccebc927023fee90c1a82d9530b08783b448e
tsa96 <tom@trumpetdust.org>
2025-02-16 14:55:35 +0000

    feat(ui): drag-drop bsp loading
    Co-author-by: Adele Reed <virepri2k@gmail.com>

--- src/Lumper.UI/Views/MainWindow.axaml.cs
+++ src/Lumper.UI/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
 namespace Lumper.UI.Views;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Disposables;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using Avalonia.ReactiveUI;
 using Lumper.UI.Services;
 using Lumper.UI.ViewModels;
@@ -19,6 +22,9 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 
         this.WhenActivated(disposables =>
         {
+            AddHandler(DragDrop.DragOverEvent, Window_OnDragOver);
+            AddHandler(DragDrop.DropEvent, Window_OnDrop);
+
             // Stupid shit to get this to behave like a radiobutton. I miss Angular
             PageService
                 .Instance.WhenAnyValue(x => x.ActivePage)
@@ -77,4 +83,19 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         if (source is not null && !source.IsChecked!.Value)
             source.IsChecked = true;
     }
+
+    private async void Window_OnDrop(object? _, DragEventArgs e)
+    {
+        List<IStorageFile> files = FilterDraggedBspFiles(e);
+        await ViewModel!.OpenDragDropped(files);
+    }
+
+    private void Window_OnDragOver(object? _, DragEventArgs e)
+    {
+        bool hasBsps = FilterDraggedBspFiles(e).Count > 0;
+        e.DragEffects = hasBsps ? DragDropEffects.Link : DragDropEffects.None;
+    }
+
+    private List<IStorageFile> FilterDraggedBspFiles(DragEventArgs e) =>
+        e.Data.GetFiles()?.OfType<IStorageFile>().Where(file => file.Name.EndsWith(".bsp")).ToList() ?? [];
 }
