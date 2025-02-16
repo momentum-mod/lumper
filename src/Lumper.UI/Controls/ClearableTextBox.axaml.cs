134b4d0d5b2c2ca794a6f49205ad8c51b7d6b6e5
tsa96 <tom@trumpetdust.org>
2025-02-16 13:04:15 +0000

    feat(ui): ClearableTextBox control

---
+++ src/Lumper.UI/Controls/ClearableTextBox.axaml.cs
@@ -0,0 +1,35 @@
+namespace Lumper.UI.Controls;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+public partial class ClearableTextBox : UserControl
+{
+    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<ClearableTextBox, string>(
+        nameof(Text),
+        defaultValue: string.Empty,
+        defaultBindingMode: Avalonia.Data.BindingMode.TwoWay
+    );
+
+    public static readonly StyledProperty<string> WatermarkProperty = AvaloniaProperty.Register<
+        ClearableTextBox,
+        string
+    >(nameof(Watermark));
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public string Watermark
+    {
+        get => GetValue(WatermarkProperty);
+        set => SetValue(WatermarkProperty, value);
+    }
+
+    public ClearableTextBox() => InitializeComponent();
+
+    private void ClearButton_OnClick(object? sender, RoutedEventArgs e) => Text = string.Empty;
+}
