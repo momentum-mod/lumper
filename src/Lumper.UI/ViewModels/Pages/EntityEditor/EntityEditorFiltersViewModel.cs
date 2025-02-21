700054f5ec315d071c93a1bc79a3d6c035b2b868
tsa96 <tom@trumpetdust.org>
2025-02-21 21:57:12 +0000

    chore(ui): full namespace name in usings
    Sorry don't have a linter for this and Rider automatically does the
    shorthand, occasionally I miss stuff

--- src/Lumper.UI/ViewModels/Pages/EntityEditor/EntityEditorFiltersViewModel.cs
+++ src/Lumper.UI/ViewModels/Pages/EntityEditor/EntityEditorFiltersViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Numerics;
-using Lib.Bsp.Struct;
+using Lumper.Lib.Bsp.Struct;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
