f5993adb00e061d8d999db36aef3417a5833fbcb
tsa96 <tom@trumpetdust.org>
2025-02-25 07:41:05 +0000

    feat(ui): map summary window

---
+++ src/Lumper.UI/ViewModels/BspInfo/BspInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lumper.UI.ViewModels.BspInfo;
+
+using Lumper.UI.Services;
+using Lumper.UI.Views.BspInfo;
+
+public sealed class BspInfoViewModel : ViewModelWithView<BspInfoViewModel, BspInfoView>
+{
+    public static BspService BspService => BspService.Instance;
+}
