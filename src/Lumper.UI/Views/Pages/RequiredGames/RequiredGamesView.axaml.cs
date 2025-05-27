216d551d8e2e298e5641dda6455f58d042ae2f13
tsa96 <tom@trumpetdust.org>
2025-05-28 00:29:58 +0100

    feat(ui): Required Games page

---
+++ src/Lumper.UI/Views/Pages/RequiredGames/RequiredGamesView.axaml.cs
@@ -0,0 +1,12 @@
+namespace Lumper.UI.Views.Pages.RequiredGames;
+
+using Avalonia.ReactiveUI;
+using Lumper.UI.ViewModels.Pages.RequiredGames;
+
+public partial class RequiredGamesView : ReactiveUserControl<RequiredGamesViewModel>
+{
+    public RequiredGamesView()
+    {
+        InitializeComponent();
+    }
+}
