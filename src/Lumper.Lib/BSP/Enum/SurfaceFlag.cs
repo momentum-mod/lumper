131088209467a3fb1e8341db8014307c9d7f7d0c
SCell555 <kubci.rusnk645@gmail.com>
2025-10-15 05:43:06 +0200

    fix(lib): Fix SurfaceFlag enum definition

--- src/Lumper.Lib/BSP/Enum/SurfaceFlag.cs
+++ src/Lumper.Lib/BSP/Enum/SurfaceFlag.cs
@@ -1,21 +1,26 @@
 namespace Lumper.Lib.Bsp.Enum;
 
-public enum SurfaceFlag
+[System.Flags]
+public enum SurfaceFlag : int
 {
-    Light,
-    Sky2d,
-    Sky,
-    Warp,
-    Trans,
-    Noportal,
-    Trigger,
-    Nodraw,
-    Hint,
-    Skip,
-    Nolight,
-    Bumplight,
-    Noshadows,
-    Nodecals,
-    Nochop,
-    Hitbox,
+    None = 0,
+    Light = 1 << 0,
+    Sky2d = 1 << 1,
+    Sky = 1 << 2,
+    Warp = 1 << 3,
+    Trans = 1 << 4,
+    Noportal = 1 << 5,
+    Trigger = 1 << 6,
+    Nodraw = 1 << 7,
+    Hint = 1 << 8,
+    Skip = 1 << 9,
+    Nolight = 1 << 10,
+    Bumplight = 1 << 11,
+    Noshadows = 1 << 12,
+    Nodecals = 1 << 13,
+    Nochop = 1 << 14,
+    Hitbox = 1 << 15,
+    SkyNoEmit = 1 << 16,
+    SkyOcclusion = 1 << 17,
+    Slick = 1 << 18,
 }
