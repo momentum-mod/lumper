6c5dfaa5fb32df6e35b13d212498a4b4c6bda31c
SCell555 <kubci.rusnk645@gmail.com>
2025-10-15 05:47:08 +0200

    feat(lib, ui): add sky occlusion surface flag job

---
+++ src/Lumper.UI/Views/Pages/Jobs/AddSkyOcclusionFlagJobView.axaml.cs
@@ -0,0 +1,12 @@
+namespace Lumper.UI.Views.Pages.Jobs;
+
+using Avalonia.ReactiveUI;
+using Lumper.UI.ViewModels.Pages.Jobs;
+
+public partial class AddSkyOcclusionFlagJobView : ReactiveUserControl<AddSkyOcclusionFlagJobViewModel>
+{
+    public AddSkyOcclusionFlagJobView()
+    {
+        InitializeComponent();
+    }
+}
